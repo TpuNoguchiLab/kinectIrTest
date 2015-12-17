9624ecb3dddb5cf83ed17392ad9a95e4c78895b1
KoheiMikami <nyatamon@gmail.com>
2015-12-18 02:56:52 +0900

    first commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,118 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetWindowShape(1920, 1080);
+	ofSetFrameRate(30);
+
+	if (!initKinect()) exit();
+
+	irImage.allocate(512, 424, OF_IMAGE_GRAYSCALE);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	IInfraredFrame *infraredFrame = nullptr;
+	HRESULT hResult = infraredReader -> AcquireLatestFrame(&infraredFrame);
+
+	if (SUCCEEDED(hResult)) {
+		//hResult = infraredFrame ->CopyFrameDataToArray(irWidth*irHeight*irBytesPerPixels, irimage.getPixels(),ColorImageFormat_Rgba);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+bool ofApp::initKinect() {
+	//senspr
+	HRESULT hResult = S_OK;
+	hResult = GetDefaultKinectSensor(&sensor);
+	if (FAILED(hResult)) {
+		std::cerr << "Error : GetDefaultKinectSensor" << std::endl;
+		return -1;
+	}
+
+	hResult = sensor->Open();
+	if (FAILED(hResult)){
+		std::cerr << "Error : IKinectSensor::Open()" << std::endl;
+		return -1;
+	}
+
+	//ir
+	hResult = sensor->get_InfraredFrameSource(&infraredSource);
+	if (FAILED(hResult)){
+		std::cerr << "Error : get_InfraredFrameSource" << std::endl;
+		return -1;
+	}
+	hResult = infraredSource->OpenReader(&infraredReader);
+	if (FAILED(hResult)){
+		std::cerr << "Error : OpenReader" << std::endl;
+		return -1;
+	}
+	hResult = infraredSource->OpenReader(&infraredReader);
+	if (FAILED(hResult)){
+		std::cerr << "Error : OpenReader" << std::endl;
+		return -1;
+	}
+
+	hResult = infraredSource->get_FrameDescription(&infraredDescription);
+	if (FAILED(hResult)){
+		std::cerr << "Error : IColorFrameSource::get_FrameDescription()" << std::endl;
+		return -1;
+	}
+
+
+	infraredDescription -> get_Width(&irWidth);
+	infraredDescription -> get_Height(&irHeight);
+	infraredDescription -> get_BytesPerPixel(&irBytesPerPixels);
+
+	return 1;
+
+};
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "ofMain.h"
+#include <kinect.h>
+
+template<class Interface>
+inline void SafeRelease(Interface *& pInterfaceToRelease)
+{
+	if (pInterfaceToRelease != NULL){
+		pInterfaceToRelease->Release();
+		pInterfaceToRelease = NULL;
+	}
+}
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+		bool initKinect();
+
+		ofImage irimage;
+
+		IKinectSensor *sensor;
+
+		IInfraredFrameSource *infraredSource;
+		IInfraredFrameReader *infraredReader;
+		IFrameDescription *infraredDescription;
+		int irWidth,irHeight;
+		unsigned int irBytesPerPixels;
+
+
+};
